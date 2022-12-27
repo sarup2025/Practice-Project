2a9a4930452452e6c37e5619b9ff266ca2bd9c95
Sarup Chakraborty <sarupmatiari@gmail.com>
2022-12-27 18:04:37 +0530

    Brick Breaker Game in Java

---
+++ src/BrickBreakerGame/MainClass.java
@@ -0,0 +1,16 @@
+package BrickBreakerGame;
+
+import javax.swing.JFrame;
+
+public class MainClass {
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        GamePlay gamePlay = new GamePlay();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("Brick Breaker Game");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
+    }
+}
